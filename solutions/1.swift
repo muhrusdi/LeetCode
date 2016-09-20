ef3d4bdceeda60451f1492255a8baf1d9b418402
yangyanzhan <yangyanzhan@gmail.com>
2016-09-20 14:44:05 +0800

    add namespace to solutions

--- solutions/1.swift
+++ solutions/1.swift
@@ -1,14 +1,18 @@
 import Foundation
 
-class SolutionLT1 {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for i in 0..<nums.count {
-            for j in (i + 1)..<nums.count {
-                if nums[i] + nums[j] == target {
-                    return [i, j]
+func solutionLT1() {
+    
+    class Solution {
+        func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+            for i in 0..<nums.count {
+                for j in (i + 1)..<nums.count {
+                    if nums[i] + nums[j] == target {
+                        return [i, j]
+                    }
                 }
             }
+            return []
         }
-        return []
     }
+    
 }
