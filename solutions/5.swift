738811df2326f4fa033c0840f89995a5825a93ce
yangyanzhan <yangyanzhan@gmail.com>
2016-09-21 17:00:18 +0800

    solution for problem 5

---
+++ solutions/5.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func solutionLC5() {
+    
+    class Solution {
+        func longestPalindrome(_ s: String) -> String {
+            let cs = [Character](s.characters)
+            var longest = ""
+            for i in 0..<cs.count {
+                var j = i, k = i
+                while 0 <= j && k < cs.count {
+                    if cs[j] != cs[k] {
+                        break
+                    }
+                    j = j - 1
+                    k = k + 1
+                }
+                j = j + 1
+                k = k - 1
+                if k - j + 1 > longest.characters.count {
+                    longest = s[s.index(s.startIndex, offsetBy: j)...s.index(s.startIndex, offsetBy: k)]
+                }
+                j = i - 1
+                k = i
+                while 0 <= j && k < cs.count {
+                    if cs[j] != cs[k] {
+                        break
+                    }
+                    j = j - 1
+                    k = k + 1
+                }
+                j = j + 1
+                k = k - 1
+                if k - j + 1 > longest.characters.count {
+                    longest = s[s.index(s.startIndex, offsetBy: j)...s.index(s.startIndex, offsetBy: k)]
+                }
+            }
+            return longest
+        }
+    }
+    
+}
